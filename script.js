5184f2d8c3aeea97d35f595e5d104d38e7ea0288
translator4geo <100690600+translator4geo@users.noreply.github.com>
2024-08-06 09:30:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+let alphabet = {
+  a: 'ა',
+  b: 'ბ',
+  c: 'ც',
+  d: 'დ',
+  e: 'ე',
+  f: 'ფ',
+  g: 'გ',
+  h: 'ჰ',
+  i: 'ი',
+  j: 'ჯ',
+  k: 'კ',
+  l: 'ლ',
+  m: 'მ',
+  n: 'ნ',
+  o: 'ო',
+  p: 'პ',
+  q: 'ქ',
+  r: 'რ',
+  s: 'ს',
+  t: 'ტ',
+  u: 'უ',
+  v: 'ვ',
+  w: 'წ',
+  x: 'ხ',
+  y: 'ყ',
+  z: 'ზ',
+  S: 'შ',
+  C: 'ჩ',
+  W: 'ჭ',
+  R: 'ღ',
+  T: 'თ',
+  Z: 'ძ',
+  '!': '!',
+  '?': '?',
+  ',': ',',
+  '.': '.',
+  // ' ': ' ',
+  // ა: 'a',
+  // ბ: 'b',
+  // გ: 'g',
+  // დ: 'd',
+  // ე: 'e',
+  // ვ: 'v',
+  // ზ: 'z',
+  // თ: 'T',
+  // ი: 'i',
+  // კ: 'k',
+  // ლ: 'l',
+  // მ: 'm',
+  // ნ: 'n',
+  // ო: 'o',
+  // პ: 'p',
+  // რ: 'r',
+  // ს: 's',
+  // ტ: 't',
+  // უ: 'u',
+  // ფ: 'f',
+  // ქ: 'q',
+  // ღ: 'R',
+  // ყ: 'y',
+  // შ: 'S',
+  // ჩ: 'C',
+  // ც: 'c',
+  // ძ: 'Z',
+  // წ: 'w',
+  // ჭ: 'W',
+  // ხ: 'x',
+  // ჯ: 'j',
+  // ჰ: 'h',
+  // '!': '!',
+  // '?': '?',
+  // ',': ',',
+  // '.': '.',
+  // ' ': ' ',
+};
+
+function generate() {
+  document.getElementById('emoji').innerHTML = '🤔';
+
+  const keys = Object.keys(alphabet);
+
+  return (document.getElementById('letter').innerHTML =
+    keys[Math.floor(Math.random() * keys.length)]);
+}
+
+function compare() {
+  let output1 = '😁';
+  let output2 = '😢';
+
+  let question = document.getElementById('letter').innerHTML;
+  let inputValue = document.getElementById('reply').value;
+  for (i = 0; i < inputValue.length; i++) {
+    if (inputValue[i] === question) {
+      document.getElementById('emoji').innerHTML = output1;
+    } else {
+      document.getElementById('emoji').innerHTML = output2;
+    }
+  }
+
+  document.getElementById('reply').value = '';
+}
